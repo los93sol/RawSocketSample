eebf52f05dcddc83686b691f9debef221430e0dc
Adam Shortland <Adam.T.Shortland@gmail.com>
2019-06-19 00:09:03 -0400

    -Adds Delay to ClientOptions

--- ClientLibrary/ClientOptions.cs
+++ ClientLibrary/ClientOptions.cs
@@ -1,9 +1,12 @@
-﻿namespace ClientLibrary
+﻿using System;
+
+namespace ClientLibrary
 {
     public class ClientOptions
     {
         public string ServerHostname { get; set; }
         public int ServerPort { get; set; }
         public string Message { get; set; }
+        public TimeSpan Delay { get; set; } = TimeSpan.FromSeconds(5);
     }
 }
